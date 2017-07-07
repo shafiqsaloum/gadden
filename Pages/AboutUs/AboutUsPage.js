e5133accf776875b041085ed8d2d2e2864fb2eaf
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-07-07 14:20:17 +0200

    Added data to about us fron page

---
+++ Pages/AboutUs/AboutUsPage.js
@@ -0,0 +1,24 @@
+var Observable = require("FuseJS/Observable");
+var data = Observable();
+
+function Article(item) {
+    this.title = item.title;
+    this.content = item.content;
+};
+
+fetch("https://dev.jexpo.se/dev/forms/aboutus?getAttributes=1")
+.then(function(response) { return response.json(); })
+.then(function(responseObject) {
+    var items = [];
+    responseObject.results.forEach(function(r) {
+        items.push(new Article(r));
+    });
+    data.replaceAll(items);
+    debug_log("data: " + JSON.stringify(data));
+}).catch(function(e) {
+    console.log("Error: " + e.message);
+});
+
+module.exports = {
+    dataSource: data
+};
